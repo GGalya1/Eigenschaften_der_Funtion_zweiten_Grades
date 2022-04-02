42414feca1066e6c7b68566ab722efaae08e34cf
GGalya <garturmail@gmail.com>
2022-04-02 17:04:03 +0200

    Projektdateien hinzufügen.

---
+++ Klasse.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAufgabe1
+{
+    class QuadratischeFunktion
+    {
+        //Variablen
+        private string Null;
+
+        //Eigenschaften
+        private double Argum1 { get; set; }
+        private double Argum2 { get; set; }
+        private double Argum3 { get; set; }
+
+        //Konstriktor
+        public QuadratischeFunktion(double _argum1, double _argum2, double _argum3)
+        {
+            Argum1 = _argum1;
+            Argum2 = _argum2;
+            Argum3 = _argum3;
+        }
+
+        //Methode
+        public string NullstellenBerechen()
+        {
+            try
+            {
+                double F = Math.Sqrt(Math.Pow(Argum2,2) - 4 * Argum1 * Argum3);
+            }
+            catch
+            {
+                return "Sie haben die Werte falsch eingegeben";
+            }
+
+            double D = Math.Sqrt(Math.Pow(Argum2, 2) - 4 * Argum1 * Argum3);
+
+   
+
+            if (D == 0)
+            {
+                try
+                {
+                    double X1 = (-Argum2 + D) / (2 * Argum1);
+                    double X2 = (-Argum2 - D) / (2 * Argum1);
+                    Null += "Die Nullstellen für die X-Achse liegen bei " + X1 + " und " + X2;
+                }
+                catch
+                {
+                    Null += "Es gibt keine Schnittpunkte beim X-Achse";
+                }
+            }
+            else
+            {
+                try
+                {
+                    double X1 = (-Argum2 + D) / (2 * Argum1);
+                    Null += "Die Nullstelle für die X-Achse liegt bei " + X1;
+                }
+                catch
+                {
+                    Null += "Es gibt keine Schnittpunkte beim X-Achse";
+                }
+            }
+
+            Null += "Die Nullstelle für die Y-Achse liegt bei " + Argum3;
+            return Null;
+        }
+
+        public string GibWerte()
+        {
+            string werte = Convert.ToString(Argum1) + " " + Convert.ToString(Argum2) + " " + Convert.ToString(Argum3);
+            return werte;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAufgabe1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Knopf_Click(object sender, RoutedEventArgs e)
+        {
+            QuadratischeFunktion f = new QuadratischeFunktion(Convert.ToDouble(Argument1.Text), Convert.ToDouble(Argument2.Text), Convert.ToDouble(Argument3.Text));
+            Ausgabe.Text = f.GibWerte();
+            Ausgabe.Text = f.NullstellenBerechen();
+        }
+    }
+}
